bf7109c13206a9ca762bc4ab1dda6cb84a276c3b
Dyraaa18 <adang.535220192@stu.untar.ac.id>
2024-05-12 15:24:38 +0700

    layouts dan register

--- app.py
+++ app.py
@@ -49,6 +49,11 @@ def get_bot_response():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'username' in session:
+        return redirect(url_for('home'))
+        
+    error_message = ""  # Inisialisasi pesan kesalahan
+
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
@@ -59,28 +64,28 @@ def register():
 
         # Cek apakah username sudah ada
         if users_collection.find_one({"username": username}):
-            error_message = "Username sudah digunakan. Silakan pilih username lain."
-            return render_template('register.html', error_message=error_message)
+            error_message = "Username already exists"
 
         # Cek apakah email sudah terdaftar
         if users_collection.find_one({"email": email}):
-            error_message = "Email sudah terdaftar. Silakan gunakan email lain."
-            return render_template('register.html', error_message=error_message)
+            error_message = "Email already exist"
 
-        # Insert data pengguna baru ke dalam koleksi
-        users_collection.insert_one({
-            "username": username,
-            "email": email,
-            "password": hashed_password
-        })
+        # Jika tidak ada pesan kesalahan, insert data pengguna baru ke dalam koleksi
+        if not error_message:
+            users_collection.insert_one({
+                "username": username,
+                "email": email,
+                "password": hashed_password
+            })
+            return redirect(url_for('login'))
 
-        return redirect(url_for('login'))
-
-    return render_template('register.html')
-
+    return render_template('register.html', error_message=error_message)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        return redirect(url_for('home'))
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
